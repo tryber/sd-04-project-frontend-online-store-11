898f7f083bddd9bc66bda54c44c09334cd3ff67d
Cezene <cezenesn@hotmail.com.br>
2020-06-17 14:54:08 -0300

    primeiro

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-//comentário cezene
+
 function App() {
   return (
     <div className="App">
@@ -22,5 +22,6 @@ function App() {
     </div>
   );
 }
+//cezene
 
 export default App;
