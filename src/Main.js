8b96fe783e0ee7c2e742cf7b12250e80cbb4e096
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-06-17 23:34:49 -0300

    initiate Component SearchBar

--- src/Main.js
+++ src/Main.js
@@ -1,1 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import SearchBar from './Searchbar';
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+    }
+  }
+
+  render() {
+    return (
+      <SearchBar searchText={this.state.searchText} />
+    )
+  }
+}
+
+export default Main;
---
+++ src/Searchbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { searchText, textChange } = this.props;
+    return (
+      <form>
+        <label htmlFor="searchText">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </label>
+        <input 
+          type="text" name="searchText" 
+          value={searchText} onChange={textChange}
+        />
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
