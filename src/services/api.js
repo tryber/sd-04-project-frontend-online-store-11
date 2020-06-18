834409b9cc47113fac7eef63552b9655c1389e21
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-06-18 17:15:06 -0300

    add 2nda API CC01

--- src/services/api.js
+++ src/services/api.js
@@ -2,13 +2,13 @@ export async function getCategories() {
   const url =
     // 'https://cors-anywhere.herokuapp.com/https://api.mercadolibre.com/sites/MLB/categories';
     'https://api.mercadolibre.com/sites/MLB/categories';
-  const categories = await fetch(url).then(res => res.json());
+  const categories = await fetch(url).then((res) => res.json());
   return categories;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // const url = `https://cors-anywhere.herokuapp.com/https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
-  const products = await fetch(url).then(res => res.json());
+  const products = await fetch(url).then((res) => res.json());
   return products;
 }
