d8da71df56696545cd893b94ec41f83e3ed8f527
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-06-19 11:27:38 -0300

    fixing cc

--- src/components/CartButton.js
+++ src/components/CartButton.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Link, Route } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa';
 import ShoppingCart from './ShoppingCart';
-import { FaShoppingCart } from 'react-icons/fa'
 
 class CartButton extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Link data-testid="shopping-cart-button" to="/shopping-cart"><FaShoppingCart size={42} /></Link>
+          <Link data-testid="shopping-cart-button" to="/shopping-cart">
+              <FaShoppingCart size={42} />
+          </Link>
         </div>
         <Switch>
           <Route path="/shopping-cart" component={ShoppingCart} />
