001a6660f703f9fbaf5a47e5f73edf00a4533050
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-06-21 00:23:52 -0300

    fixing cc

--- src/services/api.js
+++ src/services/api.js
@@ -10,6 +10,5 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // const url = `https://cors-anywhere.herokuapp.com/https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   const products = await fetch(url).then((res) => res.json());
-  console.log(products)
   return products;
 }
