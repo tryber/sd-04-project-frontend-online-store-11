7eaaac4c0f373f88c74637c4b86b77e1c131c215
Cezene <cezenesn@hotmail.com.br>
2020-06-22 16:10:08 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import MainPage from './components/MainPage';
 import ProductDetail from './components/ProductDetail';
