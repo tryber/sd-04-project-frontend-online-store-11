95f1f6b5b52dc075d9994aa476a3b779a777cce4
Cezene <cezenesn@hotmail.com.br>
2020-06-22 13:27:13 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
 import MainPage from './components/MainPage';
+import Header from './components/Header';
+import ShoppingCart from './components/ShoppingCart'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path="/" component={MainPage} />
+        <Route path="/ShoppingCart" component={ShoppingCart} />
+
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Logo from './logo.png';
+import Cart from './shop.png';
+class Header extends React.Component {
+    render() {
+        return (
+            <div class="nav">
+                <Link to='/'><img src={Logo} alt="logo"></img></Link>
+                <Link data-testid="shopping-cart-button" to='/ShoppingCart'><img src={Cart}alt="Cart" ></img></Link>
+            </div>
+        );
+    }
+}
+
+export default Header;
