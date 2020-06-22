9e87a438b1a7d971af92f1c4b9ec39046b1be01e
Cezene <cezenesn@hotmail.com.br>
2020-06-22 15:56:23 -0300

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ class Header extends React.Component {
   render() {
     return (
       <div className="nav">
-        <Link to='/'><img src={Logo} alt="logo"/></Link>
-        <Link data-testid="shopping-cart-button" to='/ShoppingCart'>
+        <Link to="/"><img src={Logo} alt="logo" /></ Link>
+        <Link data-testid="shopping-cart-button" to="/ShoppingCart">
            <img src={Cart}alt="Cart" />
          </Link>
       </div>
